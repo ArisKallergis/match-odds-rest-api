b4d52f2294aa4e37cf87f22021ce8e931a63e14b
Aristidis Kallergis <ariskallergis1@gmail.com>
2023-08-26 19:00:49 +0300

    handle validation errors

--- src/main/java/gr/accepted/matchoddsrestapi/controller/GeneralExceptionHandler.java
+++ src/main/java/gr/accepted/matchoddsrestapi/controller/GeneralExceptionHandler.java
@@ -4,35 +4,59 @@ import gr.accepted.matchoddsrestapi.exception.EntityNotFoundException;
 import gr.accepted.matchoddsrestapi.exception.IdMismatchException;
 import gr.accepted.matchoddsrestapi.model.error.ApiError;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
 
 @ControllerAdvice
 public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {IdMismatchException.class})
-    protected ResponseEntity<Object> handleIdMismatchException(IdMismatchException e, HttpServletRequest request) {
+    protected ResponseEntity<?> handleIdMismatchException(IdMismatchException e, HttpServletRequest request) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ApiError(
                         e.getMessage(),
                         e.getErrorCode(),
-                        request.getRequestURI()
+                        request.getRequestURI(),
+                        null
                 ));
 
     }
 
     @ExceptionHandler(value = {EntityNotFoundException.class})
-    protected ResponseEntity<Object> handleEntityNoFoundException(EntityNotFoundException e, HttpServletRequest request) {
+    protected ResponseEntity<?> handleEntityNoFoundException(EntityNotFoundException e, HttpServletRequest request) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ApiError(
                         e.getMessage(),
                         e.getErrorCode(),
-                        request.getRequestURI()
+                        request.getRequestURI(),
+                        null
+                ));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<String> validationErrors = e.getBindingResult().getFieldErrors()
+                .stream().map((fieldError) -> fieldError.getField() + ": " + fieldError.getDefaultMessage()).toList();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ApiError(
+                        "Invalid Input",
+                        "invalidInput",
+                        ((ServletWebRequest) request).getRequest().getRequestURI(),
+                        validationErrors
                 ));
     }
 }
--- src/main/java/gr/accepted/matchoddsrestapi/model/error/ApiError.java
+++ src/main/java/gr/accepted/matchoddsrestapi/model/error/ApiError.java
@@ -1,7 +1,10 @@
 package gr.accepted.matchoddsrestapi.model.error;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -10,4 +13,7 @@ public class ApiError {
     private String message;
     private String errorCode;
     private String requestPath;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<String> validationErrors;
 }
